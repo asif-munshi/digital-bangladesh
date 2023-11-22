238092da378318f6013ebd3b23fed51e07446e22
Asif Munshi <75932258+asif-munshi@users.noreply.github.com>
2023-11-23 00:17:37 +0600

    Setup shadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { MaxWidthWraper } from '@/components/Layout/MaxWidthWraper'
+
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div>Hello World!</div>
-    </div>
+    <MaxWidthWraper>
+      <div className="mx-auto flex max-w-3xl flex-col items-start py-20 text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 lg:text-5xl">
+          Your marketplace for high-queality{' '}
+          <span className="text-blue-600">digital assets.</span>
+        </h1>
+      </div>
+    </MaxWidthWraper>
   )
 }
