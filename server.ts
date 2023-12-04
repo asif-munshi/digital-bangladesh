c11318064cfe92b30f9fa176a7f32f171ca45e3c
Asif Munshi <75932258+asif-munshi@users.noreply.github.com>
2023-12-05 02:11:23 +0600

    added stripe webhook and receiopt email

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import * as trpcExpress from '@trpc/server/adapters/express'
+import bodyParser from 'body-parser'
 import express from 'express'
+import { IncomingMessage } from 'http'
 import { getPayloadClient } from './getPayload'
 import { nextApp, nextHandler } from './next-utils'
 import { appRouter } from './trpc'
 import { inferAsyncReturnType } from '@trpc/server'
+import { stripeWebhookHandler } from './webhooks'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -18,7 +21,19 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  })
+
+  app.post('/api/webhooks/stripe', webhookMiddleware, stripeWebhookHandler)
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
